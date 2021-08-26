631d3b62d718dcacf881c1a03ad6ded915072cd7
Meghan Bock <meghan.bock@gmail.com>
2021-08-25 20:11:18 -0500

    set up models

---
+++ models/index.js
@@ -0,0 +1,34 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id'
+});
+
+module.exports = { User, Blog, Comment };
