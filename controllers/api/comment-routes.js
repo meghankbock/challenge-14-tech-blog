75dd6747d8625e98a6b77226dda269d129873bdd
Meghan Bock <meghan.bock@gmail.com>
2021-08-25 20:37:55 -0500

    comment-routes setup

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// get all comments
+router.get('/', (req, res) => {
+    Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// create comment
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            blog_id: req.body.blog_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
+});
+
+// delete comment
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destory({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this id!'});
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
