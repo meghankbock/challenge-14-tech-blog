c91cc4365a7e3196deacff00f576bbcc576b996f
Meghan Bock <meghan.bock@gmail.com>
2021-08-25 20:46:43 -0500

    home routes setup

---
+++ controllers/home-routes.js
@@ -0,0 +1,97 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Blog, User, Comment } = require("../models");
+
+router.get("/", (req, res) => {
+  console.log("======================");
+  Blog.findAll({
+    attributes: [
+      "id",
+      "blog_content",
+      "title",
+      "created_at",
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "blog_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => {
+      const blogs = dbBlogData.map((blog) => blog.get({ plain: true }));
+      res.render("homepage", {
+        blogs,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+router.get("/blog/:id", (req, res) => {
+  Blog.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      "blog_content",
+      "title",
+      "created_at",
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "blog_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: "No blog found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const blog = dbBlogData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-blog", {
+        blog,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
