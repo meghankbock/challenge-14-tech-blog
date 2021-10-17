11c02579653472ff6ba53ec1e31c6a13dd60ccb1
Meghan Bock <meghan.bock@gmail.com>
2021-10-17 14:36:06 -0500

    rename blog to post in delete-post.js

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -5,7 +5,7 @@ async function deleteFormHandler(event) {
         window.location.toString().split('/').length - 1
     ];
 
-    const response = await fetch(`/api/blogs/${id}`, {
+    const response = await fetch(`/api/posts/${id}`, {
         method: 'DELETE'
     });
 
@@ -16,4 +16,4 @@ async function deleteFormHandler(event) {
     }
 }
 
-document.querySelector('.delete-blog-btn').addEventListener('click', deleteFormHandler);
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
