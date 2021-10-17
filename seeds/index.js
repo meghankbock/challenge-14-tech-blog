e3f279a94a21ee4d9a0ef19d93fcbc99861fa736
Meghan Bock <meghan.bock@gmail.com>
2021-10-17 15:19:44 -0500

    update seeds.js file and modify to seeds/index.js to use new seed file

--- seeds/index.js
+++ seeds/index.js
@@ -1,21 +1,25 @@
-const seedUsers = require('./user-seeds');
-const seedBlogs = require('./blog-seeds');
-const seedComments = require('./comment-seeds');
+// const seedUsers = require('./user-seeds');
+// const seedBlogs = require('./blog-seeds');
+// const seedComments = require('./comment-seeds');
+const seedData = require('./seeds');
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('--------------');
-  await seedUsers();
-  console.log('--------------');
+  await seedData();
+  console.log('---- seed data ----');
+  // await seedUsers();
+  // console.log('--------------');
 
-  await seedBlogs();
-  console.log('--------------');
+  // await seedBlogs();
+  // console.log('--------------');
 
-  await seedComments();
-  console.log('--------------');
+  // await seedComments();
+  // console.log('--------------');
 
+  console.log("all done!");
   process.exit(0);
 };
 
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,50 +1,49 @@
 const faker = require("faker");
 
-const db = require("./connection");
 const { User, Post, Comment } = require("../models");
 
-// create user data
-const userData = [];
+const seedData = () => {
+  // create user data
+  const userData = [];
 
-for (let i = 0; i < 50; i += 1) {
-  const username = faker.internet.userName(firstName, lastName);
-  const email = faker.internet.email(firstName, lastName);
-  const password = faker.internet.password();
-  userData.push({ username, email, password });
-}
+  for (let i = 0; i < 50; i += 1) {
+    const username = faker.internet.userName(firstName, lastName);
+    const email = faker.internet.email(firstName, lastName);
+    const password = faker.internet.password();
+    userData.push({ username, email, password });
+  }
 
-const createdUsers = await User.bulkCreate(userData, { individualHooks: true });
-
-// create post data
-const postData = [];
-for (let i = 0; i < 100; i += 1) {
-  const title = faker.lorem.words(Math.round(Math.random() * 9) + 1);
-  const post_content = faker.lorem.words(Math.round(Math.random() * 20) + 1);
-
-  const randomUserIndex = Math.floor(Math.random() * createdUsers.length);
-  const { id: user_id } = createdUsers[randomUserIndex];
-
-  postData.push({ title, post_content, user_id });
-
-  const createdPosts = await Post.bulkCreate(postData, {
+  const createdUsers = await User.bulkCreate(userData, {
     individualHooks: true,
   });
-}
 
-// create comment data
-const commentData = [];
-for (let i = 0; i < 100; i += 1) {
-  const comment_text = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+  // create post data
+  const postData = [];
+  for (let i = 0; i < 100; i += 1) {
+    const title = faker.lorem.words(Math.round(Math.random() * 9) + 1);
+    const post_content = faker.lorem.words(Math.round(Math.random() * 20) + 1);
 
-  const randomPostIndex = Math.floor(Math.random() * createdPosts.length);
-  const { id: post_id } = createdUsers[randomPostIndex];
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.length);
+    const { id: user_id } = createdUsers[randomUserIndex];
 
-  const randomUserIndex = Math.floor(Math.random() * createdUsers.length);
-  const { id: user_id } = createdUsers[randomUserIndex];
+    postData.push({ title, post_content, user_id });
+  }
+  const createdPosts = await Post.bulkCreate(postData);
 
-  commentData.push({ comment_text, post_id, user_id });
+  // create comment data
+  const commentData = [];
+  for (let i = 0; i < 100; i += 1) {
+    const comment_text = faker.lorem.words(Math.round(Math.random() * 20) + 1);
 
-  const createdComments = await Comment.bulkCreate(commentData, {
-    individualHooks: true,
-  });
-}
+    const randomPostIndex = Math.floor(Math.random() * createdPosts.length);
+    const { id: post_id } = createdUsers[randomPostIndex];
+
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.length);
+    const { id: user_id } = createdUsers[randomUserIndex];
+
+    commentData.push({ comment_text, post_id, user_id });
+  }
+  const createdComments = await Comment.bulkCreate(commentData);
+};
+
+module.exports = seedData;
