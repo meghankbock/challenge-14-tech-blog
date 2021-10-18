73dacb7f21f3edbe626416d70b15b18385306d99
Meghan Bock <meghan.bock@gmail.com>
2021-10-17 23:39:05 -0500

    add cookie expiration

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: process.env.SECRET,
-    cookie: {},
+    cookie: {
+        maxAge: 7200,
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
